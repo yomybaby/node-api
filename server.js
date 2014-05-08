630b4f7cf8f3541b502e232a8bde544e51e0af9f
Jong Eun Lee <yomybaby@yomybaby.com>
2014-05-08 16:24:37 +0900

    return product(bear) when after save

--- server.js
+++ server.js
@@ -43,11 +43,11 @@ router.route('/bears')
 		var bear = new Bear();		// create a new instance of the Bear model
 		bear.name = req.body.name;  // set the bears name (comes from the request)
 
-		bear.save(function(err) {
+		bear.save(function(err,bear) {
 			if (err)
 				res.send(err);
 
-			res.json({ message: 'Bear created!' });
+			res.json(product);
 		});
 
 		
